27cc31e1c8ac50c21197c89bc7a5b2b0a7cc9d7a
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-12-21 11:13:38 -0800

    2pdf

--- src/main/java/az/turingacademy/family/Main.java
+++ src/main/java/az/turingacademy/family/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         Pet pet1 = new Pet("dog", "Lessi");
-        Pet pet2 = new Pet("cat", "Miki", 3, (byte) 20, new String[]{"sleep", "jump", "drink"});
         Human mother = new Human("Lale", "Quliyeva", 1980);
         Human father = new Human("Murad", "Quliyev", 1978);
         Family family=new Family(mother,father);
@@ -15,8 +14,8 @@ public class Main {
        family.addChild(child1);
        family.addChild(child2);
        family.deleteChild(child2);
-       family.setPet(pet2);
        family.setPet(pet1);
+
         System.out.println(family);
 
 
