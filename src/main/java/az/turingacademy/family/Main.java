ce6ef0d50e17db6ff68a66d199f39675a2791b90
DilbarMirzaeva <dlbarmirza@gmail.com>
2024-12-21 11:11:33 -0800

    2pdf

--- src/main/java/az/turingacademy/family/Main.java
+++ src/main/java/az/turingacademy/family/Main.java
@@ -20,34 +20,5 @@ public class Main {
         System.out.println(family);
 
 
-
-
-//
-//        Human child1 = new Human("Nigar", "Quliyev", 2004, mother, father);
-//        Human child2 = new Human("Murad", "Quliyev", 2006, (byte) 90, pet2, mother, father,
-//                new String[][]{{"Friday", "go to course"}, {"Sunday", "go to gym"}});
-
-
-
-//        child1.greetPet();
-//
-//        child2.greetPet();
-//        child2.describePet();
-//        System.out.println("\nPet fed?");
-//        System.out.println(child1.feedPet(false));
-//        System.out.println();
-
-//        pet1.eat();
-//        pet1.respond();
-//        pet1.foul();
-//
-//        System.out.println("\nPETS toString:");
-//        System.out.println("1)" + pet1.toString());
-//        System.out.println("2)" + pet2.toString());
-//
-//        System.out.println("\nHUMAN toString");
-//        System.out.println("1)" + child1.toString());
-//        System.out.println("2)" + child2.toString());
-
     }
 }
