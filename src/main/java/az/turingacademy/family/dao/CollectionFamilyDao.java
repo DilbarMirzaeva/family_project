c2f361945d10a9af838ab323b832cf076c1fb273
DilbarMirzaeva <dlbarmirza@gmail.com>
2025-02-27 03:19:40 +0400

    FamilyDAO part

---
+++ src/main/java/az/turingacademy/family/dao/CollectionFamilyDao.java
@@ -0,0 +1,47 @@
+package az.turingacademy.family.dao;
+
+import az.turingacademy.family.human.Family;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionFamilyDao implements FamilyDao {
+    private final List<Family> families = new ArrayList<>();
+
+    @Override
+    public List<Family> getAllFamilies() {
+        return families;
+    }
+
+    @Override
+    public Family getFamilyByIndex(int index) {
+        if (index > 0 && index < families.size()) {
+            return families.get(index);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteFamily(int index) {
+        if (index > 0 && index < families.size()) {
+            families.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteFamily(Family family) {
+        return families.remove(family);
+    }
+
+    @Override
+    public void saveFamily(Family family) {
+        int index = families.indexOf(family);
+        if (index != -1) {
+            families.set(index, family);
+        } else {
+            families.add(family);
+        }
+    }
+}
---
+++ src/main/java/az/turingacademy/family/dao/FamilyDao.java
@@ -0,0 +1,13 @@
+package az.turingacademy.family.dao;
+
+import az.turingacademy.family.human.Family;
+
+import java.util.List;
+
+public interface FamilyDao {
+    List<Family> getAllFamilies();
+    Family getFamilyByIndex(int index);
+    boolean deleteFamily(int index);
+    boolean deleteFamily(Family family);
+    void saveFamily(Family family);
+}
